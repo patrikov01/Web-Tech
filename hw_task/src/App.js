d5eda1135a5cd69e58cbcfc497fddf7efea9e9c4
Georgi Patrikov <patrikov1948@gmail.com>
2023-05-13 23:18:37 +0300

    background change

--- hw_task/src/App.js
+++ hw_task/src/App.js
@@ -1,56 +1,72 @@
-import React, { useState } from 'react';  // Import React and useState hook
-import './App.css';  // Import App.css file
-import backgroundImage from './images/clouds.jfif';  // Import the background image
+import React, { useState } from 'react';
+import './App.css';
+import backgroundImage from './images/clouds.jfif';
 
-function App() {  // Define a component named "App"
-  const [fileList, setFileList] = useState([  // Declare state variables using useState hook
-    'file1.txt',
-    'file2.txt',
-    'subdir1',
-    'subdir2'
+function App() {
+  const [showFileList, setShowFileList] = useState(false);
+  const [fileList, setFileList] = useState([
+    'subdirectory1',
+    'subdir2',
+    'subdir3',
+    'subdir4',
+    'subdir5'
   ]);
 
-  function enterSubdirectory(subdirName) {  // Define a function named "enterSubdirectory"
-    const subdirIndex = fileList.indexOf(subdirName);  // Find the index of the subdirectory
-    if (subdirIndex !== -1) {  // If the subdirectory is found in the fileList
-      setFileList([  // Update the fileList state with new items
+  function enterSubdirectory(subdirName) {
+    const subdirIndex = fileList.indexOf(subdirName);
+    if (subdirIndex !== -1) {
+      setFileList([
         ...fileList.slice(0, subdirIndex),
         `${subdirName}/file1.txt`,
         `${subdirName}/file2.txt`,
         ...fileList.slice(subdirIndex + 1)
       ]);
-    } else {  // If the subdirectory is not found in the fileList
-      alert(`Subdirectory "${subdirName}" not found`);  // Show an alert message
+    } else {
+      alert(`Subdirectory "${subdirName}" not found`);
     }
   }
 
+  function handleShowFilesClick() {
+    setShowFileList(true);
+  }
+
+  function handleHideFilesClick() {
+    setShowFileList(false);
+  }
+
   return (
-    <body style={{backgroundImage: `url(${backgroundImage})`}}>  // Set the background image of the body element
-    <div className="App">  // Render a div element with a class name "App"
-      <h1>Personal Cloud</h1>  // Render a h1 element with the text "Personal Cloud"
-      <ul>  // Render an unordered list element
-        {fileList.map(item => (  // Iterate over the items in the fileList array
-          <li key={item}>  // Render a list item with a unique key
-            {item.startsWith('subdir') ? (  // If the item starts with "subdir"
-              <a href="#" onClick={() => enterSubdirectory(item)}>{item}</a>  // Render a link with an onClick event handler that calls "enterSubdirectory" function
-            ) : (  // If the item is not a subdirectory
-              item  // Render the item as is
-            )}
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={event => {  // Render a form element with an onSubmit event handler
-        event.preventDefault();  // Prevent the default form submission behavior
-        const subdirName = event.target.elements.subdirName.value;  // Get the value of the input field
-        enterSubdirectory(subdirName);  // Call the "enterSubdirectory" function with the subdirectory name
-      }}>
-        <label htmlFor="subdirName">Enter subdirectory name:</label>  // Render a label element with a text "Enter subdirectory name"
-        <input type="text" id="subdirName" name="subdirName" />  // Render an input element with a type of "text" and an id/name of "subdirName"
-        <button type="submit">Go</button>  // Render a button element with a type of "submit" and a text "Go"
-      </form>
+     <div className="App">
+      <h1>Personal Cloud</h1>
+      {showFileList ? (
+        <div>
+          <button onClick={handleHideFilesClick}>Hide files</button>
+          <ul>
+            {fileList.map(item => (
+              <li key={item}>
+                {item.startsWith('subdir') ? (
+                  <a href="#" onClick={() => enterSubdirectory(item)}>{item}</a>
+                ) : (
+                  item
+                )}
+                
+              </li>
+            ))}
+          </ul>
+          <form onSubmit={event => {
+            event.preventDefault();
+            const subdirName = event.target.elements.subdirName.value;
+            enterSubdirectory(subdirName);
+          }}>
+            <label htmlFor="subdirName">Enter subdirectory name:</label>
+            <input type="text" id="subdirName" name="subdirName" />
+            <button type="submit">Expand</button>
+          </form>
+        </div>
+      ) : (
+        <button onClick={handleShowFilesClick}>My files</button>
+      )}
     </div>
-    </body>
   );
 }
 
-export default App;  // Export the "App" component as a default export
+export default App;
