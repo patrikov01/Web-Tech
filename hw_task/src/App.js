2c509edba351d3c2aeb10709afc3a1c79571034f
Georgi Patrikov <patrikov1948@gmail.com>
2023-05-25 14:52:30 +0300

    total change

--- hw_task/src/App.js
+++ hw_task/src/App.js
@@ -1,71 +1,109 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [showFileList, setShowFileList] = useState(false);
-  const [fileList, setFileList] = useState([
-    'subdirectory1',
-    'web_tech_is_cool',
-    'abc',
-    'name',
-    'some_files'
-  ]);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
 
-  function enterSubdirectory(subdirName) {
-    const subdirIndex = fileList.indexOf(subdirName);
-    if (subdirIndex !== -1) {
-      const numFiles = Math.floor(Math.random() * 5) + 1;
-      const files = [];
-      for (let i = 1; i <= numFiles; i++) {
-        files.push(`${subdirName}/file${i}.txt`);
-      }
-      setFileList([
-        ...fileList.slice(0, subdirIndex),
-        ...files,
-        ...fileList.slice(subdirIndex + 1)
-      ]);
-    } else {
-      alert(`Subdirectory "${subdirName}" not found!`);
+  useEffect(() => {
+    fetch("/api/uploads")
+    .then(res => res.json())
+    .then(data => setUploadedFiles(data));
+    //fetchUploadedFiles();
+  }, []);
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    
+    if (!selectedFile) {
+      toast.error('Please select a file');
+      return;
     }
-  }
-  
-  function handleShowFilesClick() {
-    setShowFileList(true);
-  }
 
-  function handleHideFilesClick() {
-    setShowFileList(false);
-  }
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    try {
+      const response = await axios.post('http://localhost:3001/api/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success(response.data.message);
+      fetchUploadedFiles();
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.error);
+      } else {
+        toast.error('Failed to upload the file');
+      }
+    }
+  };
+
+  const fetchUploadedFiles = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/uploads');
+      setUploadedFiles(response.data.files);
+    } catch (error) {
+      toast.error('Failed to fetch uploaded files');
+    }
+  };
+
+  const handleFileDownload = async (filename) => {
+    try {
+      const response = await axios.get(`http://localhost:3001/api/uploads/${filename}`, {
+        responseType: 'blob',
+      });
+
+      const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = downloadUrl;
+      link.setAttribute('download', filename);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+      toast.error('Failed to download the file');
+    }
+  };
+
+  const handleFileDelete = async (filename) => {
+    try {
+      await axios.delete('http://localhost:3001/api/delete', {
+        data: { filename },
+      });
+
+      toast.success(`File "${filename}" deleted successfully!`);
+      fetchUploadedFiles();
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.error);
+      } else {
+        toast.error('Failed to delete the file');
+      }
+    }
+  };
 
   return (
-     <div className="App">
-      <h1>Personal Cloud</h1>
-      {showFileList ? (
-        <div>
-          <button onClick={handleHideFilesClick}>Hide files</button>
-          <ul>
-          {fileList.map((item) => (
-            <li key={item}>
-              <a href="#" onClick={() => enterSubdirectory(item)}>
-               {item}
-              </a>
-            </li>
-          ))}
+    <div>
+      <h1>File Upload</h1>
+      <input type="file" onChange={handleFileSelect} />
+      <button onClick={handleFileUpload}>Upload</button>
 
-          </ul>
-          <form onSubmit={event => {
-            event.preventDefault();
-            const subdirName = event.target.elements.subdirName.value;
-            enterSubdirectory(subdirName);
-          }}>
-            <label htmlFor="subdirName">Enter subdirectory name:</label>
-            <input type="text" id="subdirName" name="subdirName" />
-            <button type="submit">Expand</button>
-          </form>
-        </div>
-      ) : (
-        <button onClick={handleShowFilesClick}>My files</button>
-      )}
+      <h2>Uploaded Files:</h2>
+      <ul>
+        {uploadedFiles.map((filename) => (
+          <li key={filename}>
+            <span>{filename}</span>
+            <button onClick={() => handleFileDownload(filename)}>Download</button>
+            <button onClick={() => handleFileDelete(filename)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
